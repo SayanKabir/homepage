0979320eef54f7d823a630802dfc465439ed2567
SayanKabir <kabirsayan93@gmail.com>
2025-01-16 10:54:16 +0530

    Removed Secrets from gitignore

---
+++ lib/secrets/secrets.dart
@@ -0,0 +1,3 @@
+const UNSPLASH_API_KEY = String.fromEnvironment('UNSPLASH_API_KEY');
+const SUPABASE_PROJECT_URL = String.fromEnvironment('SUPABASE_PROJECT_URL');
+const SUPABASE_ANON_KEY = String.fromEnvironment('SUPABASE_ANON_KEY');
